8a4cac9343288a7e3e3dec882034694a2f9da737
王名旭 <1220712534@qq.com>
2017-05-08 00:06:13 +0800

    更改config

--- server/config.js
+++ server/config.js
@@ -2,9 +2,9 @@ module.exports = {
     port: '9993',
     ROUTE_BASE_PATH: '/applet',
 
-    cosAppId: 'COS_APPID',
-    cosSecretId: 'COS_SECRET_ID',
-    cosSecretKey: 'COS_SECRET_KEY',
+    cosAppId: '1253711506',
+    cosSecretId: 'AKIDVt5jR31jpvqa8URaLuIt05085vgK4FTV',
+    cosSecretKey: '2sXzd6OUN4e4JLBsNhpkW6NMANEC2g8O',
     cosFileBucket: 'album',
     cosUploadFolder: '/photos/',
 };
